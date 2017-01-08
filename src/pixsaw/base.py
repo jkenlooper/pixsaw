dbdfd1a9670f6754069d4706a0724eadb3c4acfa
Jake Hickenlooper <jake@weboftomorrow.com>
2017-01-08 15:57:24 -0700

    Shuffle piece id's

--- src/pixsaw/base.py
+++ src/pixsaw/base.py
@@ -2,6 +2,7 @@ import os.path
 from glob import glob
 import json
 import logging
+import uuid
 
 from PIL import Image
 
@@ -62,14 +63,13 @@ class Handler(object):
         logging.debug("bbox %s, %s, %s, %s" % bbox)
 
         #scan line by line for pixels that are not transparent
-        masks_count = 0
-        pieces = {}
+        masks = {}
         for row in xrange(top, bottom):
             for col in xrange(left, right):
                 if pixels[(col,row)][3] > 0:
                     mask_pixels = floodfill(pixels, bbox, (col, row))
                     # If the mask_pixels are not big enought merge to the next one that may be.
-                    if False: # TODO: merge small pieces
+                    if False: # TODO: merge small masks
                         if len(mask_pixels) < 100 and len(mask_pixels) > 1:
                             sub_flood = False # for breaking out of the for loops
                             for subrow in xrange(row, bottom):
@@ -83,6 +83,10 @@ class Handler(object):
                                             sub_flood = True
                                             break
                     if len(mask_pixels) >= 100:
+                        # Use a uuid here to avoid sequentially numbering the masks.
+                        mask_id = uuid.uuid4().hex
+
+                        # Create mask image and save under mask_id
                         maskimg = Image.new("RGBA", (width, height), (0,0,0,0))
                         pixel_seq = [(0,0,0,0) for x in range(0, (width*height))]
                         for (x, y) in mask_pixels:
@@ -92,17 +96,18 @@ class Handler(object):
                             pixels[(x,y)] = (0,0,0,0) # clear the pixel
                         maskimg.putdata( pixel_seq )
                         m_bbox = maskimg.getbbox()
-                        logging.debug("new mask: %s" % masks_count)
+                        logging.debug("new mask: %s" % mask_id)
                         maskimg.save(
                                 os.path.join(self._mask_dir,
-                                '%s%s.png' % (self.mask_prefix, masks_count)) )
+                                '%s%s.png' % (self.mask_prefix, mask_id)) )
                         maskimg.close()
 
                         #TODO: create a svg version of the mask using potrace?
-                        pieces[masks_count] = m_bbox
-                        masks_count += 1
-        piece_json_file = open(os.path.join( self._output_dir, 'pieces.json'), 'w')
-        json.dump(pieces, piece_json_file)
+                        # Save the mask bbox
+                        masks[mask_id] = m_bbox
+
+        masks_json_file = open(os.path.join( self._output_dir, 'masks.json'), 'w')
+        json.dump(masks, masks_json_file)
 
     def process(self, image):
         """Cut up the image based on the saved masks generated from the
@@ -112,20 +117,35 @@ class Handler(object):
         """
         im = Image.open(image)
         width, height = im.size
-        piece_json_file = open(os.path.join( self._output_dir, 'pieces.json'), 'r')
-        pieces = json.load(piece_json_file)
+        masks_json_file = open(os.path.join( self._output_dir, 'masks.json'), 'r')
+        masks = json.load(masks_json_file)
+        pieces = {}
+        mask_count = 0
+
+        # Rely on the glob sort ordering here to shuffle the piece int id's
         for mask in glob(os.path.join(self._mask_dir, '%s*.png' % self.mask_prefix)):
             piece = Image.new("RGBA", (width, height), (0,0,0,0))
             maskimg = Image.open(mask)
             maskname = os.path.basename(mask)
+            piecename = "{0}{1}.png".format(self.mask_prefix, mask_count)
             mask_id = maskname[len(self.mask_prefix):maskname.find('.')]
             piece.paste(im, (0,0), maskimg)
             maskimg.close()
-            logging.debug('crop %s' % pieces.get(mask_id))
-            piece = piece.crop(pieces.get(mask_id))
+            logging.debug('crop %s' % masks.get(mask_id))
+            piece = piece.crop(masks.get(mask_id))
             piece.save( os.path.join(self._raster_dir, '%s%s' %
-                (self.piece_prefix, maskname)) )
+                (self.piece_prefix, piecename)) )
             piece.save( os.path.join(self._jpg_dir, '%s%s.jpg' %
-                (self.piece_prefix, os.path.splitext(maskname)[0])) )
+                (self.piece_prefix, os.path.splitext(piecename)[0])) )
             piece.close()
+
+            # Copy the bbox from mask to pieces dict which will now have 'shuffled' int id's
+            pieces[mask_count] = masks.get(mask_id)
+            mask_count = mask_count + 1
+
         im.close()
+
+
+        # Write new pieces.json
+        pieces_json_file = open(os.path.join( self._output_dir, 'pieces.json'), 'w')
+        json.dump(pieces, pieces_json_file)
